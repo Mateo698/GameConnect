a75f0974de1e745f0d54f9831bbb58fce1641387
Sunart1 <79371501+Sunart1@users.noreply.github.com>
2024-05-11 17:14:44 -0500

    [ADD] Add nav bar and main fragments of app

--- app/src/main/java/com/gameconnect/MainActivity.kt
+++ app/src/main/java/com/gameconnect/MainActivity.kt
@@ -2,10 +2,37 @@ package com.gameconnect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.gameconnect.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        replaceFragment(Connect())
+
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.connect -> replaceFragment(Connect())
+                R.id.profile -> replaceFragment(Profile())
+                R.id.chat -> replaceFragment(Chat())
+
+                else-> {
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
     }
 }
